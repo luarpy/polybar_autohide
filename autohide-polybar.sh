f6efdfe5fe6264ef094501677a633eab092c7db5
luarpy <biurrun.121721@e.unavarra.es>
2022-01-31 23:00:50 +0100

    funciona el retorno del modo de ventana para fullscreen, screen on, screen off

--- autohide-polybar.sh
+++ autohide-polybar.sh
@@ -6,10 +6,11 @@ MARGIN_TOP=5
 MARGIN_HIDE_TOP=50
 DEBUG=1
 INTERVAL=1
-top=0
-bottom=5
-left=5
-right=5
+x=0
+y=0
+height=50
+width=1366
+margin=5
 
 VERSION='0.1'
 
@@ -20,7 +21,7 @@ Usage: autohide-polybar [action] [-p <PID>]
 Options:
   -h, --help show this message
   -p, --pid=  bars PID. Used to identify which bar must be used
-  -a, --paddings= screen margins. <top>,<right>,<bottom>,<left>. Defaults are $top, $right, $bottom, $left
+  -a, --paddings= screen margins. <x>x<y>+<height>+<width>. Defaults are $x x $y + $height + $width
 EOF
 }
 
@@ -28,17 +29,24 @@ version(){
   printf %s\\n "Version: $VERSION"
 }
 
-getPointer(){
+getPointerY(){
   local pointer="$(xdotool getmouselocation --shell | grep 'Y=' | tr -d 'Y=')"
   printf "$pointer"
 }
 
+getPointerX(){
+  local pointer="$(xdotool getmouselocation --shell | grep 'X=' | tr -d 'X=')"
+  printf "$pointer"
+}
+
 hide(){
-  polybar-msg cmd hide "$([ -n $PID ] && printf -- "-p $PID")"
+  polybar-msg cmd hide "$([ -n $PID ] && printf -- "-p $PID")"  > /dev/null
+  polybarshown=1
 }
 
 show(){
-  polybar-msg cmd show "$([ -n $PID ] && printf -- "-p $PID")"
+  polybar-msg cmd show "$([ -n $PID ] && printf -- "-p $PID")" > /dev/null
+  polybarshown=0
 }
 
 getScreenSize(){
@@ -61,9 +69,11 @@ getWindow(){
   fi
 
   if [ "$width" -ge "$screenwidth" ] && [ "$height" -ge "$screenheight" ]; then
-    printf 2
-  else
-    printf 0
+    printf 2 # Return 2 if current screen is in fullscreenmodej
+  elif [[ $width -ge $(($screenwidth - 10)) && $height -ge $(($screenheight - 5 )) ]]; then # FIXME: debería comprobar si la pantalla está en modo maximizado. Tal vez con tags de xorg o así pueda detectarse
+    printf 0 # Return 0 if current screen is maximized
+  else 
+    printf 1
   fi
 }
 
@@ -77,59 +87,48 @@ windowPresence(){
 }
 
 main(){
-  local pointer windownumber polybarshown
-  polybarshown=0
-  show
-  while :; do
+  local pointerY pointerX windownumber polybarshown
+  while true; do
     # Get initial values
-    pointer="$(getPointer)" 
+    pointerY="$(getPointerY)" 
+    pointerX="$(getPointerX)"
     windowpresence="$(windowPresence)"
 
     if [ $DEBUG -eq 0 ]; then
-      printf %s\\n "pointer Y position: $pointer" \
-        ", windowPresent: $windowpresence" \
-        ", polybarShown: $polybarshown" \
-        ", PID: $PID"
+      printf %s\\n "(x,y) pointer position: ($pointerX,$pointerY)" \
+        "windowPresent: $windowpresence" \
+        "polybarShown: $polybarshown" \
+        "PID: $PID"
     fi
-    
-    # If no windows, then show polybar
-    if [ $windowpresence -eq 1 ]; then
-        polybarshown=0
-        show
-    elif [ $windowpresence -eq 0 ]; then
-        pointer="$(getPointer)"
-        if [ $pointer -gt $MARGIN_HIDE_TOP ]; then # if mouse is under bar's area
-          # Check if window size is lower than widnowsize+margin
-          # FIXME: make this more efficient. Must not need theses amount of variables
-          local window="$(xdotool getactivewindow)" # window ID
-          window="$(xdotool getwindowgeometry --shell "$window")"
-          local x y screenwidth screenheight screensize
-          screensize="$(getScreenSize)"
-          screenwidth="$(printf "$screensize" | cut -d 'x' -f1)"
-          screenheight="$(printf "$screensize" | cut -d 'x' -f2)"
-          width="$(printf "$window" | grep 'WIDTH=' | tr -d 'WIDTH=')"
-          height="$(printf "$window" | grep 'HEIGHT=' | tr -d 'HEIGHT=')"
-          if [ $height -lt $(($screenheight - $bottom - $top)) ] && [ $width -lt $(($screenwidth - $left - $right)) ] ; then
-            poybarshow=0
-            show
-          else
-            polybarshown=1
-            hide
-          fi
-        elif [ $pointer -lt $MARGIN_TOP ]; then # if mouse is over bar's area
-          polybarshown=0
-          show
+    case "$windowpresence" in
+      # Window presence true
+      0)
+        # The pointer must be between the four points of the bar
+        if [[ $pointerX -ge $x  && \
+          $pointerY -ge $y  && \
+          $pointerX -lt $(($x + $width)) && \
+          $pointerY -lt $(($y + $height)) ]]; then
+          show 
+          echo here
+        else
+          hide
         fi
-    elif [ $windowpresence -eq 2 ]; then
-      polybarshown=1
-      hide
-    fi
+        ;;
+      # Window presence false
+      1)
+        show
+        ;;
+      # Fullscreen
+      2)
+        hide
+        ;;
+    esac
     sleep $INTERVAL
   done
   
 }
 
-params="$@"
+#params="$@"
 
 while [ "$#" -gt 0 ]; do
   param="$1"
@@ -151,20 +150,13 @@ while [ "$#" -gt 0 ]; do
       param="$1"
       PID="$param"
       ;;
-    --margin-top=)
-      shift
-      MARGIN_TOP="$1"
-      ;;
-    --margin-top-hide)
-      shift
-      MARGIN_HIDE_TOP="$1"
-      ;;
     -a|--paddings=)
       shift 
-      top="$(printf "$1" | cut -d ',' -f1)"
-      right="$(printf "$1" | cut -d ',' -f2)"
-      bottom="$(printf "$1" | cut -d ',' -f3)"
-      left="$(printf "$1" | cut -d ',' -f4)"
+      x="$(printf "$1" | cut -d 'x' -f1)"
+      y="$(printf "$1" | cut -d 'x' -f2 | cut -d '+' -f1)"
+      height="$(printf "$1" | cut -d '+' -f2)"
+      width="$(printf "$1" | cut -d '+' -f3)"
+      printf %s\\n "$x $y $height $width"
       ;;
   esac
 done
